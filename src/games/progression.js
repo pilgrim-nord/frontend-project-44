909e4b1e161f4afaadd69841bb3a1f885b32c84f
pilgrim-nord <dmelekhov@gmail.com>
2026-02-03 13:11:29 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -19,20 +19,14 @@ const makeProgression = (start, step, length) => {
 }
 
 const createTaskAndRightAnswer = () => {
-  const startNumber
-    = Math.floor(Math.random() * (MAX_START_PROGRESSION - MIN_START_PROGRESSION + 1))
-      + MIN_START_PROGRESSION;
-  const step
-    = Math.floor(Math.random() * (MAX_STEP_PROGRESSION - MIN_STEP_PROGRESSION + 1))
-      + MIN_STEP_PROGRESSION;
-  const length
-    = Math.floor(Math.random() * (MAX_PROGRESSION_LENGTH - MIN_PROGRESSION_LENGTH + 1))
-      + MIN_PROGRESSION_LENGTH;
-  const answerPosition = Math.floor(Math.random() * length);
-  const draftProgression = makeProgression(startNumber, step, length);
-  const rightAnswer = draftProgression[answerPosition];
-  draftProgression[answerPosition] = '..';
-  const progression = draftProgression.join(' ');
+  const startNumber = Math.floor(Math.random() * (MAX_START_PROGRESSION - MIN_START_PROGRESSION + 1)) + MIN_START_PROGRESSION
+  const step = Math.floor(Math.random() * (MAX_STEP_PROGRESSION - MIN_STEP_PROGRESSION + 1)) + MIN_STEP_PROGRESSION
+  const length = Math.floor(Math.random() * (MAX_PROGRESSION_LENGTH - MIN_PROGRESSION_LENGTH + 1)) + MIN_PROGRESSION_LENGTH
+  const answerPosition = Math.floor(Math.random() * length)
+  const draftProgression = makeProgression(startNumber, step, length)
+  const rightAnswer = draftProgression[answerPosition]
+  draftProgression[answerPosition] = '..'
+  const progression = draftProgression.join(' ')
 
   return {
     question: `Question: ${progression}`,
